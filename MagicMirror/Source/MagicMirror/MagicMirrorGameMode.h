82b4505e5e51b8800fd4110f21cbf4825dd78237
Leonardo Galli <leonardo.galli@bluewin.ch>
2016-04-02 20:47:13 +0200

    Added initial project

---
+++ MagicMirror/Source/MagicMirror/MagicMirrorGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "MagicMirrorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMagicMirrorGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AMagicMirrorGameMode();
+};
+
+
+
